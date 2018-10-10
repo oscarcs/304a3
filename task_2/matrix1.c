ff4b96a528e9458ce9aa43d15fce431647103789
oscarcs <nxtrafalgar@yahoo.co.nz>
2018-10-10 18:54:46 +1300

    First matrix multiplication implementation finished.

--- task_2/matrix1.c
+++ task_2/matrix1.c
@@ -29,26 +29,51 @@ int main(int argc, char* argv[]) {
     double t1, t2; 
     
     int N = 1000;
+    // int N = 3;
     
     double* A = malloc(sizeof(double) * N * N);
     double* B = malloc(sizeof(double) * N * N);
 
+    // Test (N=3):
+    // double a[] = {1, 2, 1, 0, 1, 0, 2, 3, 4};
+    // A = &a; 
+    // double b[] = {2, 5, 1, 6, 7, 1, 1, 8, 1};
+    // B = &b;
+    // &C = [15, 27, 4, 6, 7, 1, 26, 63, 9];
+
+    for (int i = 0; i < N * N; i++) {
+        A[i] = rand() % 3 + 1;
+        B[i] = rand() % 3 + 1;
+    }
+
     // Simple:
     t1 = getTime();
-    matrix_mul(A, B, N);
+    double* C = matrix_mul(A, B, N);
     t2 = getTime();    
     printf("time: %6.2f secs\n",(t2 - t1));
 
     return 0;  
 }
 
-// Simple matrix multiplication
+/**
+ * Simple matrix multiplication.
+ * Takes two single-dimensional arrays.
+ */
 double* matrix_mul(double* A, double* B, int N) {
     double* C = malloc(sizeof(double) * N * N);
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-
+            double sum = 0;
+            
+            for (int k = 0; k < N; k++) {
+                // Sum <- Sum + A(i,k) * B(k,j)
+                sum += A[N*i+k] * B[N*k+j];
+            }
+            // C(i,j) <- Sum
+            C[N*i+j] = sum;
         }
     }
+
+    return C;
 }
